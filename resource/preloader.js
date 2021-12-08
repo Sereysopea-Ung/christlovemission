e0931eee8449f2a176fbf53abf7dd5fd4082618c
unknown <ungsereysopeacontact@gmail.com>
2021-12-09 06:40:55 +0700

    modify index.html add nodemon

--- resource/preloader.js
+++ resource/preloader.js
@@ -4,4 +4,10 @@ let loader_url = [
     'resource/preloader-green-blue.gif'
     ];
 let random_number = Math.floor(Math.random()*3);
-document.getElementById('preloader').style.background = `#fff url(${loader_url[random_number]}) no-repeat center center`;
+
+
+document.getElementById('preloader').style.background = `#0d1321 url(${loader_url[random_number]}) no-repeat center center`;
+document.getElementById('preloader').style.backgroundSize = '15%'; 
+
+
+
